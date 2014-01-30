d36f4b6428af0cde9a3f2eba83eeff3b63567985
carlio <git@carlcrowder.com>
2014-01-30 17:11:30 +0100

    Configuring static and media root from environment

--- butler/butler/settings.py
+++ butler/butler/settings.py
@@ -93,6 +93,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.environ.get('STATIC_ROOT', os.path.join(BASE_DIR, 'static'))
+MEDIA_ROOT = os.environ.get('MEDIA_ROOT', os.path.join(BASE_DIR, 'static'))
 
 
 # Celery settings
