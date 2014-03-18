f3f109175dae1bed1784a2f90a4e3c1ebd6427d7
carlio <git@carlcrowder.com>
2014-03-18 16:10:32 +0100

    [refs #2] Adding AJAXy goodness to the system overview page

--- butler/systems/views.py
+++ butler/systems/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+import json
+from systems.models import Machine
 
-# Create your views here.
+
+class MachineAjaxUpdate(View):
+
+    def get(self, _):
+        data = {}
+        for machine in Machine.objects.all():
+            data[machine.id] = {
+                'security': machine.security_packages,
+                'total': machine.total_packages,
+                'reboot': machine.requires_restart
+            }
+
+        return HttpResponse(content=json.dumps(data), content_type='application/json')
